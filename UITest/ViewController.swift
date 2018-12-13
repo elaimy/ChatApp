bd795b66a122c77f29ce5b515b7058b5f7148de3
Ahmed El-elaimy <ahmedel-elaimy@ahmeds-macbook-pro.local>
2018-12-13 11:55:12 +0200

    Initial Commit

---
+++ UITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITest
+//
+//  Created by Ahmed El-elaimy on 12/13/18.
+//  Copyright © 2018 Ahmed El-elaimy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
